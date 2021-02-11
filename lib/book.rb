8022a00db7d98117a132a7cb40b92044237a19a6
Zachary <kcaz311@gmail.com>
2021-02-11 09:03:16 -0500

    commit tree 9a3c08aa9a1d03f91ac097c6d31f3b9922b1866d

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,11 @@
-
+class Book
+    def initialize(title)
+        @title = title
+    end
+    attr_accessor :page_count, :genre, :author
+    attr_reader :title
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
